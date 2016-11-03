dd17655c9592395db375e8fde3b0d8b3160e238f
JeffSAnderson <jmanderson99@comcast.net>
2016-11-03 14:58:28 -0500

    Week 3 Coding HW
    The lexical scoping assignment with a cached matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This "magic" function will input a matrix and store it in the parent 
+## environment in the correct format for lexical scoping
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+  
 }
 
-
-## Write a short comment describing this function
+## mat = matrix(1:4,2,2)
+## myMatrix = makeCacheMatrix(mat)
+## cacheSolve(myMatrix)
+## This function will execute the actual calling functions and use solve to calculate the inverse matrix solution
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+    m <- x$getinv()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
+  
 }
+## setwd("C:\\Users\\Jeff\\Documents\\GitHub\\ProgrammingAssignment2")
